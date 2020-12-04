f4d8d92b7a84e7f561ccbd8f18888d0b8575838e
KamataOrchestra <75162517+KamataOrchestra@users.noreply.github.com>
2020-12-04 23:28:36 +0900

    Add files via upload

---
+++ fade.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal('#towel',{
+    duration: 2000,
+    scale:2,
+    delay:200
+});
+
+ScrollReveal().reveal('.about-img',{
+    origin:'left',
+    duration: 2000,
+    opacity:0.5,
+    delay:200
+});
+ScrollReveal().reveal('.about-text',{
+    duration: 1000,
+    delay:150,
+});
