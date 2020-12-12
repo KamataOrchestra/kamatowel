a13448dc0dd8b458fd9fc5bef7d40394dbc4341b
KamataOrchestra <75162517+KamataOrchestra@users.noreply.github.com>
2020-12-12 19:51:58 +0900

    Add files via upload

--- fade.js
+++ fade.js
@@ -7,10 +7,40 @@ ScrollReveal().reveal('#towel',{
 ScrollReveal().reveal('.about-img',{
     origin:'left',
     duration: 2000,
-    opacity:0.5,
+    opacity:0.3,
     delay:200
 });
 ScrollReveal().reveal('.about-text',{
     duration: 1000,
-    delay:150,
+    delay:200,
+});
+ScrollReveal().reveal('.article-contents1',{
+    duration: 1000,
+    delay:200,
+    origin:'left',
+    distance: '200px',
+});
+ScrollReveal().reveal('.article-contents2',{
+    duration: 1000,
+    delay:200,
+    origin:'right',
+    distance: '200px',
+});
+ScrollReveal().reveal('.article-contents3',{
+    duration: 1000,
+    delay:200,
+    origin:'left',
+    distance: '200px',
+});
+ScrollReveal().reveal('.article-text1',{
+    duration: 1000,
+    delay:500,
+});
+ScrollReveal().reveal('.article-text2',{
+    duration: 1000,
+    delay:500,
+});
+ScrollReveal().reveal('.article-text3',{
+    duration: 1000,
+    delay:500,
 });
